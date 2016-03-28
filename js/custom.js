5cca134ef15dbe1792b8a86933f721e40a502186
Александр <raa.2010@mail.ru>
2016-03-28 23:17:45 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(window).load(function(){
+
+    /*Yandex maps*/
+    ymaps.ready(init);
+    var map;
+
+    function init(){
+        map = new ymaps.Map("home-map", {
+            center: [55.76, 37.64],
+            zoom: 6
+        });
+        map.behaviors.disable('scrollZoom');
+    }
+
+});
+
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
